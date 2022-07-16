44f4be25a71e38e59b16784fe11022d9f5c3e3fc
Wesley Rafael <wesleyrafael_p@outlook.com>
2022-07-15 21:45:05 -0300

    process

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,11 +7,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost', 
+        host: 'us-cdbr-east-06.cleardb.net', 
         port: 3306,
-        username: 'root',
-        password: 'example',
-        database: 'WrVideos',
+        username: 'b8c66d0e60e4fc',
+        password: '763d8948',
+        database: 'heroku_4341986e5ee966c',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
